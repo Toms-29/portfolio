a0c557d896cb37a7803f1ecc7023d088fb5505de
unknown <gamezmatiastomas@gmail.com>
2024-10-21 15:52:46 -0300

    Completando el contenido de la "seccion Sobre mi" y dandole estilo en Main.jsx

--- src/components/PRESENTATION/Main.jsx
+++ src/components/PRESENTATION/Main.jsx
@@ -10,15 +10,23 @@ function Main() {
                 <section className='w-11/12 flex flex-col items-center gap-5 sm:w-3/4'>
                     {/* Sobre mi */}
                     <article className='text-center tracking-wider md:max-w-2xl'>
-                        <h2>Sobre mi:</h2>
+                        <h1 className=' text-3xl font-semibold'>¡Hola soy
+                            <span className='text-teal-600 text-3xl font-semibold'> Tomás</span>!
+                        </h1>
                         <p>
-                            ¡Hola! Soy Tomás y estoy muy motivado por empezar a desarrollar código como Front-End junto a vos.
-                            He adquirido conocimientos sólidos en JavaScript y React. Estoy entusiasmado por renovar y
-                            desarrollar nuevos proyectos.
+                            y desarrollo hace mas de dos años con <span className='text-yellow-400'> JavaScript </span>
+                             y <span className='text-sky-500'> React </span>, aplicando buenas practicas.
+                            Igualmente, continuo mejorando mis habilidades como dev, manteniendo me actualizado sobre las tecnologias y
+                            utilizando herramientas que hagan mas dinamico el trabajo con las mismas.
                         </p>
                         <br />
                         <p>
-                            Estoy listo para enfrentar desafíos profesionalmente.
+                            Ya que el mundo IT crece de manera rapida y constante, uno debe mantenerce informado para no quedarce en el camino,
+                            en mi caso me matengo actualizado con newslwtters como Daily.dev y comunidades de devs como FrontendCafe.
+                        </p>
+                        <br />
+                        <p>
+                            Estoy entusiasmado por enfrentar desafíos profesionalmente y desarrollar nuevos proyectos.
                             Si estás interesado, no dudes en contactarme, sería un placer contribuir al crecimiento de tu equipo,
                             aportar mis habilidades y seguir creciendo en un entorno colaborativo.
                             Estoy convencido de que juntos podemos crear soluciones innovadoras y marcar la diferencia en cada proyecto.
