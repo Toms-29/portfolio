250e87d4778cb9f487185e533b3831d3830917a1
unknown <gamezmatiastomas@gmail.com>
2024-10-21 14:29:07 -0300

    Mejoras de los estilos de Header.jsx

--- src/components/CONTAINER/DownloadButton.jsx
+++ src/components/CONTAINER/DownloadButton.jsx
@@ -5,7 +5,7 @@ const DownloadButton = () => {
 
     return (
         <>
-            <button className='py-3 px-5 rounded-3xl font-bold text-sky-500 bg-white hover:text-white hover:bg-sky-500'>
+            <button className='py-3 px-5 rounded-3xl font-bold text-teal-600 bg-white hover:text-white hover:bg-teal-600 '>
                 <a href='../../../public/assets/files/ejemplo_cv.txt' download>
                     Descargar CV
                 </a>
