67e1c1bc061c3c3cc5193ee6cedec3269dd99547
unknown <gamezmatiastomas@gmail.com>
2024-10-21 12:25:31 -0300

    Alineacion de los items en el nav - Estilacion del header

--- src/components/PRESENTATION/Header.jsx
+++ src/components/PRESENTATION/Header.jsx
@@ -4,8 +4,8 @@ function Header() {
 
     return (
         <>
-            <header className="w-full flex h-16 bg-zinc-900 justify-center items-center">
-                <nav className="w-11/12 sm:w-3/4">
+            <header className="w-full flex h-20 bg-zinc-900 justify-center items-center border-4 border-b-teal-600 border-r-0 border-t-0 border-l-0">
+                <nav className="w-11/12 sm:w-3/4 flex justify-end">
                     <Link
                         link='https://github.com/Toms-29'
                         imgSrc='../../../public/assets/images/github.png'
