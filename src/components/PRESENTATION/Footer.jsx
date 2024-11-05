b6cd39c10c6a778dd4b35f6927d8a5969f94204f
unknown <gamezmatiastomas@gmail.com>
2024-11-05 10:55:26 -0300

    Footer.jsx disenio responsive para dispositivos chicos

--- src/components/PRESENTATION/Footer.jsx
+++ src/components/PRESENTATION/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
 
     return (
         <>
-            <footer className='w-full h-48 bg-gray-900 flex justify-center items-center'>
+            <footer className='w-full xl:h-48 bg-gray-900 flex justify-center items-center'>
                 <section className='w-11/12 sm:w-3/4 flex flex-row-reverse items-center justify-between gap-3 my-5'>
                     <article className='flex flex-col items-center justify-center gap-1'>
                         <h3 className='text-white'>Contacto: </h3>
@@ -14,21 +14,21 @@ function Footer() {
                                 link='https://wa.me/541150955469'
                                 imgSrc='./assets/images/whatsapp.png'
                                 boxStyle='w-12'
-                                imgStyle='w-12 hover:drop-shadow-green'
+                                imgStyle='w-10 md:w-12 hover:drop-shadow-green'
                                 arialLabel='Link a WhatsApp'>
                             </Link>
                             <Link
                                 link='https://mail.google.com/mail/?view=cm&fs=1&to=gamezmatiastomas@gmail.com'
                                 imgSrc='./assets/images/gmail.png'
                                 boxStyle='w-12'
-                                imgStyle='w-12 hover:drop-shadow-pink'
+                                imgStyle='w-10 md:w-12 hover:drop-shadow-pink'
                                 arialLabel='Link a Gmail'>
                             </Link>
                             <Link
                                 link='https://www.linkedin.com/in/tomasgamez/'
                                 imgSrc='./assets/images/linkedin.png'
                                 boxStyle='w-12'
-                                imgStyle='w-12 hover:drop-shadow-sky'
+                                imgStyle='w-10 md:w-12 hover:drop-shadow-sky'
                                 arialLabel='Link a LinkedIn'>
                             </Link>
                         </section>
